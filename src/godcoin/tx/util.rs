cfe6b671e0a425dff19280b5054803fef7eb115a
samrg472 <samrg472@gmail.com>
2018-08-01 14:29:03 -0700

    Inline tx util functions

--- src/godcoin/tx/util.rs
+++ src/godcoin/tx/util.rs
@@ -3,6 +3,7 @@ macro_rules! tx_deref {
         impl ::std::ops::Deref for $ty {
             type Target = Tx;
 
+            #[inline(always)]
             fn deref(&self) -> &Tx {
                 &self.base
             }
@@ -13,12 +14,14 @@ macro_rules! tx_deref {
 macro_rules! tx_sign {
     ($ty:ty) => {
         impl SignTx for $ty {
+            #[inline]
             fn sign(&self, key_pair: &KeyPair) -> SigPair {
                 let mut buf = Vec::new();
                 self.encode(&mut buf);
                 key_pair.sign(&buf)
             }
 
+            #[inline]
             fn append_sign(&mut self, key_pair: &KeyPair) {
                 let pair = self.sign(key_pair);
                 self.base.signature_pairs.push(pair);
