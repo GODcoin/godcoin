0f205fc9bde8beec91154b84de23d4f39119f694
samrg472 <samrg472@gmail.com>
2018-10-24 11:26:18 -0700

    Add synchronizer skeleton

--- crates/godcoin/src/godcoin/producer/synchronizer.rs
+++ crates/godcoin/src/godcoin/producer/synchronizer.rs
@@ -1,1 +1,22 @@
-// TODO
+use tokio::prelude::*;
+use net::peer_pool::*;
+use std::sync::Arc;
+use blockchain::*;
+
+pub struct Synchronizer {
+    chain: Arc<Blockchain>,
+    pool: Arc<PeerPool>
+}
+
+impl Synchronizer {
+    pub fn new(chain: Arc<Blockchain>, pool: Arc<PeerPool>) -> Synchronizer {
+        Synchronizer {
+            chain,
+            pool
+        }
+    }
+
+    pub fn start(&self) {
+        // TODO
+    }
+}
