cc7a95c1f4c21f36417e6740bfaf89029fb3ec2e
samrg472 <samrg472@gmail.com>
2019-03-01 14:47:57 -0800

    Use u8 for message ID len

--- crates/p2p/src/codec.rs
+++ crates/p2p/src/codec.rs
@@ -3,7 +3,9 @@ use std::io::{Error, ErrorKind};
 use bytes::{BufMut, BytesMut};
 
 // 5 MiB limit
-const MAX_PAYLOAD_LEN: u32 = 5_242_880;
+const MAX_MSG_LEN: u32 = 5_242_880;
+// 512 bits should be way more than enough
+const MAX_ID_LEN: u8 = 64;
 
 #[derive(Debug)]
 pub struct Payload {
@@ -13,7 +15,7 @@ pub struct Payload {
 
 #[derive(Default, Debug)]
 pub struct Codec {
-    id_len: u32,
+    id_len: u8,
     msg_len: u32
 }
 
@@ -28,12 +30,13 @@ impl Encoder for Codec {
     type Error = Error;
 
     fn encode(&mut self, item: Self::Item, buf: &mut BytesMut) -> Result<(), Error> {
+        assert!(item.id.len() < usize::from(MAX_ID_LEN));
+        assert!((item.msg.len() as u32) < MAX_MSG_LEN);
         {
-            let total_len = item.id.len() + item.msg.len() + 8;
-            assert!((total_len as u32) < MAX_PAYLOAD_LEN);
-            buf.reserve(8 + total_len);
+            let total_len = item.id.len() + item.msg.len() + 5;
+            buf.reserve(total_len + 5);
         }
-        buf.put_u32_be(item.id.len() as u32);
+        buf.put_u8(item.id.len() as u8);
         buf.put_u32_be(item.msg.len() as u32);
         buf.put(item.id);
         buf.put(item.msg);
@@ -46,16 +49,18 @@ impl Decoder for Codec {
     type Error = Error;
 
     fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>, Error> {
-        if self.id_len == 0 && self.msg_len == 0 && buf.len() >= 8 {
-            let buf = buf.split_to(8);
-            self.id_len = u32::from_be_bytes([buf[0], buf[1], buf[2], buf[3]]);
-            self.msg_len = u32::from_be_bytes([buf[4], buf[5], buf[6], buf[7]]);
-            if self.id_len + self.msg_len > MAX_PAYLOAD_LEN {
-                return Err(Error::new(ErrorKind::Other, format!("payload must be <={} bytes", MAX_PAYLOAD_LEN)))
+        if self.id_len == 0 && self.msg_len == 0 && buf.len() >= 5 {
+            let buf = buf.split_to(5);
+            self.id_len = buf[0];
+            self.msg_len = u32::from_be_bytes([buf[1], buf[2], buf[3], buf[4]]);
+            if self.id_len > MAX_ID_LEN {
+                return Err(Error::new(ErrorKind::Other, format!("id must be <={} bytes", MAX_ID_LEN)))
+            } else if self.msg_len > MAX_MSG_LEN {
+                return Err(Error::new(ErrorKind::Other, format!("msg must be <={} bytes", MAX_MSG_LEN)))
             }
         }
         if self.id_len != 0 && self.msg_len != 0
-                && buf.len() >= (self.id_len + self.msg_len) as usize {
+                && buf.len() >= usize::from(self.id_len) + (self.msg_len as usize) {
             let id_len = self.id_len;
             self.id_len = 0;
 
@@ -96,7 +101,7 @@ mod tests {
             msg: vec![255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245].into()
         }, &mut bytes).unwrap();
         assert_eq!(bytes, vec![
-            0, 0, 0, 6, // ID len
+            6, // ID len
             0, 0, 0, 11, // Msg len
             0, 1, 2, 3, 4, 5, // ID
             255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245 // Msg
@@ -107,7 +112,7 @@ mod tests {
     fn test_decode_full_frame() {
         let mut codec = Codec::new();
         let mut bytes = BytesMut::from(vec![
-            0, 0, 0, 6, // ID len
+            6, // ID len
             0, 0, 0, 11, // Msg len
             0, 1, 2, 3, 4, 5, // ID
             255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245 // Msg
@@ -122,7 +127,7 @@ mod tests {
     fn test_decode_multipart_frame() {
         let mut codec = Codec::new();
         let mut bytes = BytesMut::from(vec![
-            0, 0, 0, 6, // ID len
+            6, // ID len
             0, 0, 0, 11, // Msg len
             0, 1, 2, 3, 4, 5, // ID
             255, 254, 253, 252, 251, 250, 249, 248 // Msg
@@ -141,18 +146,18 @@ mod tests {
     fn test_decode_multiple_multipart_frames() {
         let mut codec = Codec::new();
         let mut bytes = BytesMut::from(vec![
-            0, 0, 0, 6, // ID len
+            6, // ID len
             0, 0, 0, 11, // Msg len
             0, 1, 2, 3, 4, 5, // ID
             255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245, // Msg
-            0, 0, 0, 3 // ID len
+            3 // ID len
         ]);
         assert_decode!(codec, bytes,
                 vec![0, 1, 2, 3, 4, 5],
                 vec![255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245]);
 
         // Ensure the codec data is correct
-        assert_eq!(bytes.len(), 4);
+        assert_eq!(bytes.len(), 1);
         assert_eq!(codec.id_len, 0);
         assert_eq!(codec.msg_len, 0);
 
@@ -180,17 +185,17 @@ mod tests {
         let mut codec = Codec::new();
         let mut bytes = BytesMut::from(vec![
             // First payload
-            0, 0, 0, 6, // ID len
+            6, // ID len
             0, 0, 0, 11, // Msg len
             0, 1, 2, 3, 4, 5, // ID
             255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245, // Msg
             // Second payload
-            0, 0, 0, 3,
+            3,
             0, 0, 0, 4,
             1, 1, 1,
             2, 2, 2, 2,
             // Third payload
-            0, 0, 0, 4,
+            4,
             0, 0, 0, 5,
             3, 3, 3, 3,
             4, 4, 4, 4, 4
