c3a025a122a3d8275dc1afa372a78fe82ee093ca
samrg472 <samrg472@gmail.com>
2019-03-14 10:32:01 -0700

    Move codec to the protocol module

--- crates/p2p/src/lib.rs
+++ crates/p2p/src/lib.rs
@@ -2,14 +2,14 @@ pub mod metrics;
 pub mod network;
 pub mod peer;
 
-mod codec;
+mod protocol;
 mod server;
 
-pub use codec::Payload;
+pub use protocol::Payload;
 pub use metrics::*;
 pub use network::{cmd, Network};
 pub use peer::{Peer, PeerId, PeerInfo};
 
 use actix::prelude::*;
-use codec::Codec;
+use protocol::Codec;
 use log::{debug, error, warn};
