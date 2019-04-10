ca18d58d969857b17302492a4b59d9ec0489645d
samrg472 <samrg472@gmail.com>
2019-04-10 11:06:23 -0700

    Initialize blockchain on server

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,6 +1,11 @@
 use actix_web::{middleware, server, App, HttpRequest, HttpResponse};
 use env_logger::{Env, DEFAULT_FILTER_ENV};
-use std::env;
+use godcoin::prelude::*;
+use log::info;
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
 fn index(_: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().body("Hello world")
@@ -10,8 +15,44 @@ fn main() {
     env_logger::init_from_env(Env::new().filter_or(DEFAULT_FILTER_ENV, "godcoin=info,actix=info"));
     godcoin::init().unwrap();
 
-    let sys = actix::System::new("godcoin-server");
+    let home: PathBuf = {
+        let home = {
+            use dirs;
+            match env::var("GODCOIN_HOME") {
+                Ok(s) => PathBuf::from(s),
+                Err(_) => Path::join(&dirs::data_local_dir().unwrap(), "godcoin"),
+            }
+        };
+        if !Path::is_dir(&home) {
+            let res = std::fs::create_dir(&home);
+            res.unwrap_or_else(|_| panic!("Failed to create dir at {:?}", &home));
+            info!("Created GODcoin home at {:?}", &home);
+        } else {
+            info!("Found GODcoin home at {:?}", &home);
+        }
+        home
+    };
 
+    let minter_key = {
+        let key =
+            env::var("GODCOIN_MINTER_KEY").expect("GODCOIN_MINTER_KEY is required to init server");
+        PrivateKey::from_wif(&key).expect("GODCOIN_MINTER_KEY is invalid")
+    };
+
+    let blockchain = Blockchain::new(&home);
+    info!(
+        "Using height in block log at {}",
+        blockchain.get_chain_height()
+    );
+
+    {
+        let create_genesis = blockchain.get_block(0).is_none();
+        if create_genesis {
+            blockchain.create_genesis_block(&minter_key);
+        }
+    }
+
+    let sys = actix::System::new("godcoin-server");
     server::HttpServer::new(|| {
         App::new()
             .middleware(middleware::Logger::new(r#"%a "%r" %s %T"#))
