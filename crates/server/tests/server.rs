93cc394e52772c9e7237a3fc74996db316598d6a
samrg472 <samrg472@gmail.com>
2019-05-07 14:29:56 -0700

    Split integration tests

--- crates/server/tests/server.rs
+++ crates/server/tests/server.rs
@@ -3,29 +3,7 @@ use godcoin::prelude::*;
 
 mod common;
 
-use common::TestMinter;
-
-#[test]
-fn fresh_blockchain() {
-    System::run(|| {
-        let minter = TestMinter::new();
-        let chain = minter.chain();
-        assert!(chain.get_block(0).is_some());
-        assert_eq!(chain.get_chain_height(), 0);
-
-        let owner = chain.get_owner();
-        assert_eq!(owner.minter, minter.genesis_info().minter_key.0);
-        assert_eq!(
-            owner.script,
-            script::Builder::new().push(OpFrame::False).build()
-        );
-        assert_eq!(owner.wallet, (&minter.genesis_info().script).into());
-
-        assert!(chain.get_block(1).is_none());
-        System::current().stop();
-    })
-    .unwrap();
-}
+pub use common::TestMinter;
 
 #[test]
 fn get_block() {
