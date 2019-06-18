94d454040cfcd0792d24129f2e7eda0f00be271f
samrg472 <samrg472@gmail.com>
2019-06-18 15:20:37 -0700

    Assert the from address in transfers have their balances subtracted

--- crates/server/tests/transfer_tx.rs
+++ crates/server/tests/transfer_tx.rs
@@ -8,12 +8,13 @@ pub use common::*;
 fn basic_transfer() {
     System::run(|| {
         let minter = TestMinter::new();
+        let from_addr = ScriptHash::from(&minter.genesis_info().script);
         let to_addr = KeyPair::gen();
 
         let create_tx = |fee: &str, amount: Asset| {
             let mut tx = TransferTx {
                 base: create_tx_header(TxType::TRANSFER, fee),
-                from: (&minter.genesis_info().script).into(),
+                from: from_addr.clone(),
                 to: (&to_addr.0).into(),
                 amount,
                 memo: vec![],
@@ -24,8 +25,9 @@ fn basic_transfer() {
             TxVariant::TransferTx(tx)
         };
 
-        let bal = get_asset("1.0000 GRAEL");
-        let tx = create_tx("1.0000 GRAEL", bal);
+        let from_bal = minter.chain().get_balance(&from_addr, &[]).unwrap();
+        let amount = get_asset("1.0000 GRAEL");
+        let tx = create_tx("1.0000 GRAEL", amount);
         let fut = minter.request(MsgRequest::Broadcast(tx));
         System::current().arbiter().send(
             fut.and_then(move |res| {
@@ -35,7 +37,12 @@ fn basic_transfer() {
             .and_then(move |minter| {
                 let chain = minter.chain();
                 let cur_bal = chain.get_balance(&to_addr.0.into(), &[]);
-                assert_eq!(cur_bal, Some(bal));
+                assert_eq!(cur_bal, Some(amount));
+
+                // The fee transfers back to the minter wallet in the form of a reward tx so it
+                // must not be subtracted during the assertion
+                let cur_bal = chain.get_balance(&from_addr, &[]);
+                assert_eq!(cur_bal, from_bal.sub(amount));
 
                 System::current().stop();
                 Ok(())
