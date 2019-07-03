e7f869f874e6f7a34aca950b9cd6ff6d894ab797
samrg472 <samrg472@gmail.com>
2019-07-03 11:23:56 -0700

    Use toml config for minter key and bind address

--- crates/server/src/bin/server/main.rs
+++ crates/server/src/bin/server/main.rs
@@ -2,18 +2,40 @@ use clap::{App, Arg};
 use env_logger::{Env, DEFAULT_FILTER_ENV};
 use godcoin::{blockchain::ReindexOpts, prelude::*};
 use log::info;
+use serde::Deserialize;
 use std::{
     env, fs,
     path::{Path, PathBuf},
 };
 
+#[derive(Debug, Deserialize)]
+struct Config {
+    minter_key: String,
+    bind_address: Option<String>,
+}
+
 fn main() {
     env_logger::init_from_env(Env::new().filter_or(DEFAULT_FILTER_ENV, "godcoin=info,actix=info"));
     godcoin::init().unwrap();
 
+    let home = {
+        match env::var("GODCOIN_HOME") {
+            Ok(s) => PathBuf::from(s),
+            Err(_) => Path::join(&dirs::data_local_dir().unwrap(), "godcoin"),
+        }
+    };
+
+    let home = home.to_string_lossy();
     let args = App::new("godcoin-server")
         .about("GODcoin core server daemon")
         .version(env!("CARGO_PKG_VERSION"))
+        .arg(
+            Arg::with_name("home")
+                .long("home")
+                .default_value(&home)
+                .empty_values(false)
+                .help("Home directory which defaults to env var GODCOIN_HOME"),
+        )
         .arg(
             Arg::with_name("reindex")
                 .long("reindex")
@@ -26,13 +48,8 @@ fn main() {
         )
         .get_matches();
 
+    let home = PathBuf::from(args.value_of("home").expect("Failed to obtain home path"));
     let (blocklog_loc, index_loc) = {
-        let home = {
-            match env::var("GODCOIN_HOME") {
-                Ok(s) => PathBuf::from(s),
-                Err(_) => Path::join(&dirs::data_local_dir().unwrap(), "godcoin"),
-            }
-        };
         if !Path::is_dir(&home) {
             let res = std::fs::create_dir(&home);
             res.unwrap_or_else(|_| panic!("Failed to create dir at {:?}", &home));
@@ -45,13 +62,16 @@ fn main() {
         (blocklog_loc, index_loc)
     };
 
-    let minter_key = {
-        let key =
-            env::var("GODCOIN_MINTER_KEY").expect("GODCOIN_MINTER_KEY is required to init server");
-        PrivateKey::from_wif(&key).expect("GODCOIN_MINTER_KEY is invalid")
-    };
+    let config_file = Path::join(&home, "config.toml");
+    info!("Opening configuration file at {:?}", config_file);
+    let config_file = fs::read(config_file).expect("Failed to open config");
+    let config: Config = toml::from_str(&String::from_utf8(config_file).unwrap()).unwrap();
 
-    let bind_addr = env::var("GODCOIN_BIND_ADDR").unwrap_or_else(|_| "127.0.0.1:7777".to_owned());
+    let minter_key =
+        PrivateKey::from_wif(&config.minter_key).expect("Provided minter key is invalid");
+    let bind_addr = config
+        .bind_address
+        .unwrap_or_else(|| "127.0.0.1:7777".to_owned());
 
     let reindex = if args.is_present("reindex") {
         info!("User requested reindexing");
