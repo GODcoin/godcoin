89c142b6b0142b925fbd80f8eb066a1063ca3940
samrg472 <samrg472@gmail.com>
2019-07-30 14:37:53 -0700

    Use hex numbers for the network msg type

--- crates/godcoin/src/godcoin/net.rs
+++ crates/godcoin/src/godcoin/net.rs
@@ -110,11 +110,16 @@ impl ResponseType {
 
 #[repr(u8)]
 pub enum MsgType {
-    Error = 0,
-    Broadcast = 10,
-    GetProperties = 20,
-    GetBlock = 21,
-    GetAddressInfo = 22,
+    // Returned to clients when an error occurred processing a request
+    Error = 0x01,
+
+    // Operations that can update the blockchain state
+    Broadcast = 0x10,
+
+    // Getters
+    GetProperties = 0x20,
+    GetBlock = 0x21,
+    GetAddressInfo = 0x22,
 }
 
 #[derive(Clone, Debug, PartialEq)]
