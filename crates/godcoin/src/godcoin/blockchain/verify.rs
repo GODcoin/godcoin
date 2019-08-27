cb85f2a1d5bf0eed882b35dda3bdd42f4c2ab42e
samrg472 <samrg472@gmail.com>
2019-08-27 10:28:37 -0700

    Use hex for TxErr serialization

--- crates/godcoin/src/godcoin/blockchain/verify.rs
+++ crates/godcoin/src/godcoin/blockchain/verify.rs
@@ -39,27 +39,27 @@ impl TxErr {
     pub fn serialize(self, buf: &mut Vec<u8>) {
         match self {
             TxErr::ScriptEval(err) => {
-                buf.push(0);
+                buf.push(0x00);
                 buf.push_u32(err.pos);
                 buf.push(err.err as u8);
             }
-            TxErr::ScriptHashMismatch => buf.push(1),
-            TxErr::ScriptRetFalse => buf.push(2),
-            TxErr::Arithmetic => buf.push(3),
-            TxErr::InsufficientBalance => buf.push(4),
-            TxErr::InvalidFeeAmount => buf.push(5),
-            TxErr::TooManySignatures => buf.push(6),
-            TxErr::TxTooLarge => buf.push(7),
-            TxErr::TxProhibited => buf.push(8),
-            TxErr::TxExpired => buf.push(9),
-            TxErr::TxDupe => buf.push(10),
+            TxErr::ScriptHashMismatch => buf.push(0x01),
+            TxErr::ScriptRetFalse => buf.push(0x02),
+            TxErr::Arithmetic => buf.push(0x03),
+            TxErr::InsufficientBalance => buf.push(0x04),
+            TxErr::InvalidFeeAmount => buf.push(0x05),
+            TxErr::TooManySignatures => buf.push(0x06),
+            TxErr::TxTooLarge => buf.push(0x07),
+            TxErr::TxProhibited => buf.push(0x08),
+            TxErr::TxExpired => buf.push(0x09),
+            TxErr::TxDupe => buf.push(0x0A),
         }
     }
 
     pub fn deserialize(cursor: &mut Cursor<&[u8]>) -> io::Result<Self> {
         let tag = cursor.take_u8()?;
         Ok(match tag {
-            0 => {
+            0x00 => {
                 let pos = cursor.take_u32()?;
                 let kind = match cursor.take_u8()? {
                     t if t == EvalErrType::UnexpectedEOF as u8 => EvalErrType::UnexpectedEOF,
@@ -78,16 +78,16 @@ impl TxErr {
                 };
                 TxErr::ScriptEval(EvalErr::new(pos, kind))
             }
-            1 => TxErr::ScriptHashMismatch,
-            2 => TxErr::ScriptRetFalse,
-            3 => TxErr::Arithmetic,
-            4 => TxErr::InsufficientBalance,
-            5 => TxErr::InvalidFeeAmount,
-            6 => TxErr::TooManySignatures,
-            7 => TxErr::TxTooLarge,
-            8 => TxErr::TxProhibited,
-            9 => TxErr::TxExpired,
-            10 => TxErr::TxDupe,
+            0x01 => TxErr::ScriptHashMismatch,
+            0x02 => TxErr::ScriptRetFalse,
+            0x03 => TxErr::Arithmetic,
+            0x04 => TxErr::InsufficientBalance,
+            0x05 => TxErr::InvalidFeeAmount,
+            0x06 => TxErr::TooManySignatures,
+            0x07 => TxErr::TxTooLarge,
+            0x08 => TxErr::TxProhibited,
+            0x09 => TxErr::TxExpired,
+            0x0A => TxErr::TxDupe,
             _ => {
                 return Err(io::Error::new(
                     io::ErrorKind::InvalidData,
