eaf82343da1af7fb29859441762ba3c2d7bd9f0c
samrg472 <samrg472@gmail.com>
2019-11-01 10:29:25 -0700

    Print P2SH key for cli keypair gen command

--- crates/cli/src/keypair.rs
+++ crates/cli/src/keypair.rs
@@ -1,10 +1,11 @@
-use godcoin::prelude::{KeyPair, Wif};
+use godcoin::prelude::{KeyPair, Wif, ScriptHash};
 
 pub fn generate_keypair() {
     let pair = KeyPair::gen();
     println!("~~ Keys have been generated ~~");
     println!("Private key WIF: {}", pair.1.to_wif());
     println!("Public key WIF: {}", pair.0.to_wif());
+    println!("P2SH key WIF: {}", ScriptHash::from(pair.0).to_wif());
     println!("- Make sure the keys are securely stored");
     println!("- Coins cannot be recovered if you lose your private key");
     println!("- Never give private keys to anyone");
