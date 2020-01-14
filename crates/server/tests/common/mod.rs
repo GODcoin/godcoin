a82683a2e51fce2adf6e57bcbf8ba78db0bbeb2f
samrg472 <samrg472@gmail.com>
2020-01-14 07:59:15 -0800

    Increase default expiry time for integration tests
    During reindexing, the transaction may expire, this causes the tx
    manager to (correctly) purge the tx from the database. By increasing the
    default expiry, we can ensure that the tx will remain in the db for as
    long as necessary.

--- crates/server/tests/common/mod.rs
+++ crates/server/tests/common/mod.rs
@@ -8,7 +8,7 @@ pub fn get_asset(grael: &str) -> Asset {
 }
 
 pub fn create_tx_header(fee: &str) -> Tx {
-    let expiry = godcoin::get_epoch_time() + 1;
+    let expiry = godcoin::get_epoch_time() + 30;
     create_tx_header_with_expiry(fee, expiry)
 }
 
